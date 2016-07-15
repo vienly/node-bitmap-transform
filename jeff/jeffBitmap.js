f2339d180617ba23cbf9c5002df7eed0c4f81fe1
Jeffrey Gebhardt <jeff@gebbey.com>
2016-07-15 09:29:54 -0700

    added buffers

---
+++ jeff/jeffBitmap.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+fs.readFile('./palette-bitmap.bmp', function (err, data) {
+  if (err) return err;
+  console.log('Byte length: ' + Buffer.byteLength(data));
+
+  console.log('Complete File: ');
+  console.log(data);
+
+  var commonHeader = data.slice(0, 13);
+  console.log('Common Header: ');
+  console.log(commonHeader);
+
+  var bitmapHeader = data.slice(14, 67);
+  console.log('Bitmap Header: ');
+  console.log(bitmapHeader);
+
+  var pallete = data.slice(68, 1077);
+  console.log('Palette: ');
+  console.log(pallete);
+
+  fs.writeFile('./output.txt', data, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('The file was written!');
+  });
+});
