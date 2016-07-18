389fed8665eb2b158071a0ddf36707cb7b19fe06
Jeffrey Gebhardt <jeff@gebbey.com>
2016-07-18 13:12:29 -0700

    fixed broken code

--- test/test.js
+++ test/test.js
@@ -45,7 +45,7 @@ describe('bitmap transform "grayscale"', function(){
     var testResult = test.readUInt32LE(1200);
     var testResult2 = test.readUInt32LE(1600);
     console.log(testResult);
-    var transformed = fs.readFileSync('./test/resources/pikaGrayscalePhotoshop2.bmp');
+    var transformed = fs.readFileSync('./test/resources/pikaGrayscalePhotoshop.bmp');
     var transformedResult = transformed.readUInt32LE(1200);
     var transformedResult2 = transformed.readUInt32LE(1600);
     console.log(transformedResult);
